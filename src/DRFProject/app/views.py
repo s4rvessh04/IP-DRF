828299bf370200a9e040cb44855c5624968a7c26
targusrock <targusrock@gmail.com>
2022-01-05 15:06:25 +0530

    Added: Home and Login templates for testing

--- src/DRFProject/app/views.py
+++ src/DRFProject/app/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse("Home")
+    context = {
+        "message": "This is Homepage"
+    }
+    return render(request, 'index.html', context=context)
     
 def login_view(request):
-    return HttpResponse("LoginForm")
+    context = {
+        "message": "This is login page"
+    }
+    return render(request, 'login.html', context=context)
